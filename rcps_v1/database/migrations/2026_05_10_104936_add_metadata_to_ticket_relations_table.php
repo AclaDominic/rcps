445c4e30916346835f210060ca1d29b7aa4b7a20
random <random@gmail.com>
2026-05-10 10:50:27 +0800

    fix: deployasdfasefasfsawef

---
+++ rcps_v1/database/migrations/2026_05_10_104936_add_metadata_to_ticket_relations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('ticket_relations', function (Blueprint $table) {
+            if (!Schema::hasColumn('ticket_relations', 'metadata')) {
+                $table->text('metadata')->nullable();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('ticket_relations', function (Blueprint $table) {
+            $table->dropColumn('metadata');
+        });
+    }
+};
